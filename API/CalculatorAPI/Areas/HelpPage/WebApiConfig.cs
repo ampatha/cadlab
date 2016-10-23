f7f8ed500140e940fba35d209c551e141b537236
Ahmed Sabbour <sabb0ur@gmail.com>
2016-10-23 12:57:40 +0300

    Added exception telemetry with Application Insights

--- API/CalculatorAPI/Areas/HelpPage/WebApiConfig.cs
+++ API/CalculatorAPI/Areas/HelpPage/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CalculatorAPI.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace CalculatorAPI
 {
@@ -26,6 +28,9 @@ namespace CalculatorAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Track Exceptions with Application Insights
+            config.Services.Add(typeof(IExceptionLogger), new AiExceptionLogger());
         }
     }
 }
